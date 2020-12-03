b3eb982cf1eba6eda480a91403a66b60c3b6f6ed
macazure <38785252+macazure@users.noreply.github.com>
2020-12-03 15:21:35 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,11 @@
 variable "appId" {
   description = "Azure Kubernetes Service Cluster service principal"
+  type        =   string
+  default     = "69a8c671-f14e-4195-b3ed-9f9cc13e7b09"
 }
 
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
+  type        =   string
+  default     = "LesB7jU7_3aIL3O8YlMhpY~2S72BJuFm~e"
 }
